44144eb5deb82e3b7b6904ecc16448f705bb5951
ramandeep1995 <66180911+ramandeep1995@users.noreply.github.com>
2021-08-26 14:23:24 -0400

    Create pay.php

---
+++ pay.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GamingSpot</title>
+    <link href="hf.css" rel="stylesheet" type="text/css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+    
+    <style>
+        .form-check-input[type=checkbox].filled-in:checked+label:after,
+        label.btn input[type=checkbox].filled-in:checked+label:after {
+            border: 2px solid #4285f4;
+            background-color: #4285f4;
+        }
+        .mb-4-5 {
+            margin-bottom: 24px;
+        }
+        body{
+            background-image: url("images/5.jpg");
+            background-size: cover;
+        }
+
+    </style>
+
+</head>
+
+<header>
+    <nav >
+        <img src="images/Logo.png" height="60px" />
+
+        <ul style="padding: 25px">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+            <li><a href="home.php">Logout</a></li>
+
+        </ul>
+
+    </nav>
+</header>
+<body>
+<div class="container mt-4 mb-4">
+    <div class="row d-flex cart align-items-center justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+
+                <div class="row g-0">
+                    <div class="col-md-6 border-right p-5">
+                        <div class="text-center order-details">
+                            <div class="d-flex justify-content-center mb-5 flex-column align-items-center">
+                                <span class="check1"><i class="fas fa-check fa-5x"></i></span>
+                                <span class="font-weight-bold" style="font-size: 40px">Order Confirmed</span>
+                                <small class="mt-2">Thank You For Shopping at GamingSpot</small> </div>
+                            <button class="btn btn-danger rounded-pill py-2 btn-block"><a href="home.php" style="color: white">Go to Home</a></button>
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+                <div> </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer style="position: relative">
+
+    <a href="aboutus.php" style="color: black">About Us</a>
+
+    <a href="https://goo.gl/maps/AA5Na8QjBoht4SAFA" style="color: black">Addresses</a>
+    <a href="#" style="color: black" onclick="return false;">Contact-+1433-222-2213</a>
+</footer>
+</html>
