502e311e0d1f42329718ab09447be724808f69e3
tejashvi1 <69606663+tejashvi1@users.noreply.github.com>
2021-08-26 15:00:34 -0400

    Add files via upload

---
+++ deletebutton.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+
+require_once "config.php";
+
+
+$id = $_GET['id'];
+
+$del = mysqli_query($mysqli, "delete from users where id = '$id'");
+
+if ($del) {
+    mysqli_close($mysqli);
+    header("location:user.php");
+    exit;
+} else {
+    echo "Error deleting record";
+}
+
+
+?>
