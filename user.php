68a83fcf7aca14c153ee953bd4ecb46b2eeacc96
urveerana <urveerana01@gmail.com>
2021-08-26 20:04:53 -0400

    Update user.php

--- user.php
+++ user.php
@@ -15,7 +15,7 @@ require_once ("config.php");
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 
-
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
    
 
     <style>
