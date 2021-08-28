a144c247570439f0c665ffee718cf380561be970
tejashvi1 <69606663+tejashvi1@users.noreply.github.com>
2021-08-28 18:11:28 -0400

    aboutus.php
    this is our aboutus page

---
+++ aboutus.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GamingSpot</title>
+    <link href="hf.css" rel="stylesheet" type="text/css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        .form-check-input[type=checkbox].filled-in:checked+label:after,
+        label.btn input[type=checkbox].filled-in:checked+label:after {
+            border: 2px solid #4285f4;
+            background-color: #4285f4;
+        }
+
+        body{
+            background-image: url("images/5.jpg");
+            background-size: cover;
+        }
+
+    </style>
+
+</head>
+
+<header style="position: relative">
+    <nav >
+        <img src="images/Logo.png" height="60px" />
+
+        <ul style="padding: 25px">
+            <li><a href="home.php">Home</a></li>
+        </ul>
+
+    </nav>
+</header>
+<body>
+
+<div class="bg-light">
+    <div class="container py-5">
+        <div class="row h-100 align-items-center py-5">
+            <div class="col-lg-6">
+                <h1 class="display-4" style="text-align: left">About us</h1>
+                <p class="lead text-muted mb-0">GamingSpot Inc</p>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/illus_kftyh4.png" alt="" class="img-fluid"></div>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white py-5">
+    <div class="container py-5">
+        <div class="row align-items-center mb-5">
+            <div class="col-lg-6 order-2 order-lg-1">
+                <h2 class="font-weight-light" style="text-align: left">Our Purpose</h2>
+                <p class="font-italic text-muted mb-4">GamingSpot purpose is to compete in e-commerce world at low cost. All the games available at this site has help the gamers to buy at cheap cost.</p>
+            </div>
+            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="images/10.jfif" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-5 px-5 mx-auto"><img src="images/11.jfif" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+            <div class="col-lg-6">
+                <h2 class="font-weight-light">Our Objective</h2>
+                <p class="font-italic text-muted mb-4">The project is aimed at developing an All-In-One website for E-commerce game store. The online gaming store provides an easy interface for users to shop their favourite games according to different categories. </p>
+            </div>
+        </div>
+    </div>
+</div>
